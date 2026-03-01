13c03e9dfa94c589bea197c538b6647c579267a3
Kyle Anderson <kyle.anderson@cpaohio.org>
2026-03-01 16:36:46 -0500

    updated

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,24 +1,38 @@
 import java.util.Scanner;
 
 public class ShipCostCalculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    static void main() {
+        Scanner in = new Scanner(System.in);
+
+        double itemPrice = 0;
+        double shippingCost = 0;
+        double totalPrice = 0;
+        String trash = "";
 
         System.out.print("Enter the price of the item: $");
-        double itemPrice = scanner.nextDouble();
 
-        double shippingCost;
-        if (itemPrice >= 100) {
-            shippingCost = 0;
-        } else {
-            shippingCost = itemPrice * 0.02;
+        if (in.hasNextDouble()) {
+            itemPrice = in.nextDouble();
+            in.nextLine();
+
+            if (itemPrice >= 100) {
+                shippingCost = 0;
+            }
+            else {
+                shippingCost = itemPrice * 0.02;
+            }
+
+            totalPrice = itemPrice + shippingCost;
+
+            System.out.printf("Shipping Cost: $%.2f%n", shippingCost);
+            System.out.printf("Total Price:   $%.2f%n", totalPrice);
+        }
+        else {
+            trash = in.nextLine();
+            System.out.println("\nYou entered: " + trash);
+            System.out.println("Run the program again and enter a valid amount!");
         }
 
-        double totalPrice = itemPrice + shippingCost;
-
-        System.out.printf("Shipping Cost: $%.2f%n", shippingCost);
-        System.out.printf("Total Price:   $%.2f%n", totalPrice);
-
-        scanner.close();
+        in.close();
     }
 }
